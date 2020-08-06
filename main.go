01f5cc22e5b106a5b524a0c58d56c921d8943a0c
Matan Shalit <matan.shalit@everphone.de>
2020-08-06 17:13:19 +0200

    fix: not crapping out on errors + closing connections

--- main.go
+++ main.go
@@ -18,7 +18,10 @@ var targetPort string
 // non env global vars
 var targetAddress string
 
-func pipe(reader io.Reader, writer io.Writer) {
+func pipe(reader, writer net.Conn) {
+	defer func() { _ = reader.Close() }()
+	defer func() { _ = writer.Close() }()
+
 	for {
 		buffer := make([]byte, 1024)
 		n, err := reader.Read(buffer)
@@ -27,19 +30,22 @@ func pipe(reader io.Reader, writer io.Writer) {
 			if err == io.EOF {
 				break // gracefully exiting
 			}
-			log.Fatalf("%+v", errors.Wrap(err, "failed reading data"))
+			log.Printf("%+v", errors.Wrap(err, "failed reading data"))
+			break
 		}
 		_, err = writer.Write(data)
 		if err != nil {
-			log.Fatalf("%+v", errors.Wrap(err, "failed writing data"))
+			log.Printf("%+v", errors.Wrap(err, "failed writing data"))
+			break
 		}
 	}
+	log.Printf("connection %s ended", reader.RemoteAddr())
 }
 
 func handleConnection(readConn net.Conn) {
 	writeConn, err := net.Dial("tcp", targetAddress)
 	if err != nil {
-		log.Fatalf("%+v", errors.Wrapf(err, "failed to create a new connection to target %s", targetAddress))
+		log.Printf("%+v", errors.Wrapf(err, "failed to create a new connection to target %s", targetAddress))
 	}
 	go pipe(readConn, writeConn)
 	go pipe(writeConn, readConn)
@@ -62,7 +68,7 @@ func main() {
 	for {
 		conn, err := ln.Accept()
 		if err != nil {
-			log.Fatalf("%+v", errors.Wrapf(err, "failed to accept a connection on %s", address))
+			log.Printf("%+v", errors.Wrapf(err, "failed to accept a connection on %s", address))
 		}
 		log.Printf("got a connection from %s", conn.RemoteAddr())
 		go handleConnection(conn)
